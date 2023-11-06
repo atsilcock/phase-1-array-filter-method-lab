f2a69160481978d0e1a66fa8517450f4543e41f1
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2023-11-06 12:37:59 -0700

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// Code your solution
+function findMatching(names, target){
+    const result = names.filter((driver)=> driver.toLowerCase() === target.toLowerCase())
+    return result
+}
+function fuzzyMatch(names, target){
+    const result = names.filter((driver)=>driver.startsWith(target))
+    return result
+}
+
+function matchName(names, target){
+    const result = names.filter((driver)=> driver.name==target)
+    return result
+}
